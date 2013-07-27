19aab447ca144aa3f8942f76f3d69b173d41f9dd
Brian Slesinsky <brian-git@slesinsky.org>
2013-07-26 22:40:16 -0700

    move lzw compression to a separate file and clean up a bit

--- lib/gifencoder.dart
+++ lib/gifencoder.dart
@@ -1,6 +1,7 @@
 library gifencoder;
 
 import "dart:typed_data";
+import "src/lzw.dart" as lzw;
 
 // Spec: http://www.w3.org/Graphics/GIF/spec-gif89a.txt
 // Explanation: http://www.matthewflickinger.com/lab/whatsinagif/bits_and_bytes.asp
@@ -13,7 +14,7 @@ const maxColors = 1<<maxColorBits;
 class IndexedImage {
   final int width;
   final int height;
-  final colors = new ColorTable();
+  final colors = new _ColorTable();
   List<int> pixels;
 
   /**
@@ -35,7 +36,7 @@ class IndexedImage {
         _header(width, height, colors.bits)
         ..addAll(colors.table)
         ..addAll(_startImage(0, 0, width, height))
-        ..addAll(_pixels(pixels, colors.bits))
+        ..addAll(lzw.compress(pixels, colors.bits))
         ..addAll(_trailer()));
   }
 }
@@ -43,7 +44,7 @@ class IndexedImage {
 class IndexedAnimation {
   final int width;
   final int height;
-  final colors = new ColorTable();
+  final colors = new _ColorTable();
   final frames = new List<List<int>>();
   
   IndexedAnimation(this.width, this.height, List<List<int>> rgbaFrames) {
@@ -71,14 +72,14 @@ class IndexedAnimation {
       bytes
         ..addAll(_delayNext(delay))
         ..addAll(_startImage(0, 0, width, height))
-        ..addAll(_pixels(frame, colors.bits));
+        ..addAll(lzw.compress(frame, colors.bits));
     }
     bytes.addAll(_trailer());
     return new Uint8List.fromList(bytes);
   }
 }
 
-class ColorTable {
+class _ColorTable {
   final List<int> table = new List<int>();
   final colorToIndex = new Map<int, int>();
   int bits;
@@ -166,35 +167,6 @@ List<int> _startImage(int left, int top, int width, int height) {
   return bytes;
 }
 
-/**
- * Compresses the pixels using LZW.
- */
-List<int> _pixels(List<int> pixels, int colorBits) {
-  CodeBook book = new CodeBook(colorBits);
-  CodeBuffer buf = new CodeBuffer(book);
-  
-  buf.add(book.clearCode);
-  if (pixels.isEmpty) {
-    buf.add(book.endCode);
-    return buf.finish();    
-  }
-  
-  int code = pixels[0];
-  for (int px in pixels.sublist(1)) {
-    int newCode = book.codeAfterAppend(code, px);
-    if (newCode == null) {
-      buf.add(code);
-      book.define(code, px);
-      code = px;
-    } else {
-      code = newCode;
-    }
-  }
-  buf.add(code);    
-  buf.add(book.endCode);
-  return buf.finish();
-}
-
 List<int> _trailer() {
   return [0x3b];
 }
@@ -206,127 +178,4 @@ void _addShort(List<int> dest, int n) {
   dest..add(n & 0xff)..add(n >> 8);
 }
 
-// The highest code that can be defined in the CodeBook.
-const maxCode = (1 << 12) - 1;
 
-/**
- * A CodeBook contains codes defined during LZW compression. It's a mapping from a string
- * of pixels to the code that represents it. The codes are stored in a trie which is
- * represented as a map. Codes may be up to 12 bits. The size of the codebook is always
- * the minimum power of 2 needed to represent all the codes and automatically increases
- * as new codes are defined.
- */
-class CodeBook {
-  int colorBits;
-  // The "clear" code which resets the table.
-  int clearCode;
-  // The "end of data" code.
-  int endCode;
-
-  // A mapping from (c1, pixel) -> c2 that returns the new code for the pixel string
-  // formed by appending a pixel to the end of c1's pixel string. (In addition, the
-  // codes for single pixels are stored in the map with c1 set to 0.)
-  // The key is encoded by shifting c1 to the left by eight bits and adding the pixel,
-  // forming a 20-bit number.
-  Map<int, int> _codeAfterAppend;
-
-  // Codes from this value and above are not yet defined.
-  int nextUnused;
-  
-  // The number of bits required to represent every code.
-  int bitsPerCode;
-  
-  // The current size of the codebook.
-  int size;
-  
-  CodeBook(this.colorBits) {
-    if (colorBits < 2) {
-      colorBits = 2;
-    }
-    assert(colorBits <= 8);
-    clearCode = 1 << colorBits;
-    endCode = clearCode + 1;
-    clear();
-  }
-  
-  void clear() {
-    _codeAfterAppend = new Map<int, int>();
-    nextUnused = endCode + 1;
-    bitsPerCode = colorBits + 1;
-    size = 1 << bitsPerCode;
-  }
-  
-  /**
-   * Returns the new code after appending a pixel to the pixel string represented by the previous code,
-   * or null if the code isn't in the table.
-   */
-  int codeAfterAppend(int code, int pixelIndex) {
-   return _codeAfterAppend[(code << 8) | pixelIndex];
-  }
-
-  /**
-   * Defines a new code to be the pixel string of a previous code with one pixel appended.
-   * Returns true if defined, or false if there's no more room in the table.
-   */
-  bool define(int code, int pixelIndex) {
-    if (nextUnused == maxCode) {
-      return false;
-    }
-    _codeAfterAppend[(code << 8) | pixelIndex] = nextUnused++;
-    if (nextUnused > size) {
-      bitsPerCode++;
-      size = 1 << bitsPerCode;
-    }
-    return true;
-  }
-}
-
-/// Writes a sequence of integers using a variable number of bits, for LZW compression.
-class CodeBuffer {
-  final CodeBook book;
-  final finishedBytes = new List<int>();
-  // A buffer containing bits not yet added to finishedBytes.
-  int buf = 0;
-  // Number of bits in the buffer.
-  int bits = 0;
-
-  CodeBuffer(this.book);
-  
-  void add(int code) {
-    assert(code >= 0 && code < book.size);
-    buf |= (code << bits);
-    bits += book.bitsPerCode;
-    while (bits >= 8) {
-      finishedBytes.add(buf & 0xFF);
-      buf = buf >> 8;
-      bits -= 8;
-    }
-  }
-  
-  List<int> finish() {
-    // Add the remaining bits. (Unused bits are set to zero.)
-    if (bits > 0) {
-      finishedBytes.add(buf);
-    }
-    
-    // The final result starts withe the number of color bits.
-    final dest = new List<int>();
-    dest.add(book.colorBits);
-
-    // Divide it up into blocks with a size in front of each block.
-    int len = finishedBytes.length;
-    for (int i = 0; i < len;) {
-      if (len - i >= 255) {
-        dest.add(255);
-        dest.addAll(finishedBytes.sublist(i, i + 255));
-        i += 255;
-      } else {
-        dest.add(len - i);
-        dest.addAll(finishedBytes.sublist(i, len));
-        i = len;
-      }
-    }
-    dest.add(0);
-    return dest;
-  }
-}
