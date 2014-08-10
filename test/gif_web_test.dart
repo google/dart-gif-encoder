83b6fa54ccc7fa717893f1bcd0b93129ed72e546
Brian Slesinsky <brian-git@slesinsky.org>
2014-08-09 22:33:50 -0700

    got unit test passing again

--- test/gif_web_test.dart
+++ test/gif_web_test.dart
@@ -7,14 +7,14 @@ import 'dart:html';
 
 main() {
   useHtmlConfiguration();
-  
+
   test('one black pixel', () {
     var ctx = new CanvasElement(width: 1, height: 1).context2D ..fillStyle = "black" ..fillRect(0, 0, 1, 1);
     var data = ctx.getImageData(0, 0, 1, 1);
     var blob = new Blob([gif.makeGif(data.width, data.height, data.data)]);
-    
+
     var f = new FileReader();
-    
+
     loadedDataUrl(ProgressEvent e) {
       expect(f.error, null);
       expect(f.readyState, FileReader.DONE);
@@ -22,14 +22,14 @@ main() {
       expect(url, startsWith("data:;"));
       url = url.replaceFirst("data:;", "data:image/gif;");
       var img = new ImageElement(src: url);
-      img.onLoad.listen(expectAsync1((e) {
+      img.onLoad.listen(expectAsync((e) {
         expect(img.width, 1);
         expect(img.height, 1);
-        checkPixel(img, 0, 0, [0, 0, 0, 255]);        
+        checkPixel(img, 0, 0, [0, 0, 0, 255]);
       }));
     }
-    
-    f.onLoadEnd.listen(expectAsync1(loadedDataUrl));
+
+    f.onLoadEnd.listen(expectAsync(loadedDataUrl));
     f.readAsDataUrl(blob);
   });
 }
@@ -38,5 +38,5 @@ checkPixel(ImageElement img, int x, int y, List<int> expectedRGBA) {
   var ctx = new CanvasElement(width: x + 1, height: y + 1).context2D ..fillStyle = "white" ..fillRect(0, 0, x + 1, y + 1);
   ctx.drawImage(img, 0, 0);
   var data = ctx.getImageData(0, 0, 1, 1).data;
-  expect(data, expectedRGBA);         
+  expect(data, expectedRGBA);
 }
